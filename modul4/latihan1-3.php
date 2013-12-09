a49db59191aef83d4be8505a0a366157a757d754
Muhammad Luqman Hakim <soroiku@yahoo.co.id>
2013-12-09 21:36:05 +0700

    upload ulang
    upload ulang laporan praktikum 1 sampai terakhir

---
+++ modul4/latihan1-3.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hapus Cookies</title>
+</head>
+
+<body>
+<?php
+setcookie('nama_cookie', 'nilai_cookie');
+if (isset($_COOKIE['nama cookie'])) {
+	echo 'cookie di-set <br />';
+	
+	//Menghapus cookie, dengan masa berlaku 3 jam yang lalu
+	setcookie('nama_cookie', '', time() - 3 * 3600);
+	echo 'cookie dihapus';
+} else {
+	echo 'unset';
+}
+
+?>
+
+<p>
+Tekan Refresh (F5);
+
+</body>
+</html>
